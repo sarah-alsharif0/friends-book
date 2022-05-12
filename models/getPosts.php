5a7c43500a88b1a0c2497b1fbe9c7fb7a1e0571d
Sarah Alsharif <197163@ppu.edu.ps>
2022-05-12 21:37:37 +0300

    modified getUserPosts

--- models/getPosts.php
+++ models/getPosts.php
@@ -3,7 +3,6 @@
 	function getUserAndFriendsPosts($userId){
 
 		require dirname(__DIR__)."/util/dbconnection.php";
-		require dirname(__DIR__)."/models/getPostInteractions.php";
 		require dirname(__DIR__)."/components/post.php";
 
 		$query = "SELECT friends.`user1-id` , friends.`user2-id`, post.* 
@@ -35,44 +34,25 @@
 	function getUserPosts($userId){
 
 		require dirname(__DIR__)."/util/dbconnection.php";
-		require dirname(__DIR__)."/models/getPostInteractions.php";
+		require dirname(__DIR__)."/components/post.php";
 
-		$query = "SELECT `first-name` , `last-name`, `image-url` FROM user WHERE id=$userId";
+		$query = "SELECT * FROM post WHERE `user-id`= $userId ORDER BY `date` DESC";
+
 		$result = mysqli_query($conn,$query);
 
 		if($result){
-			$rowInfo = mysqli_fetch_assoc($result);
+			if(mysqli_num_rows($result) > 0 ){
+				$posts = "";
+				while($row = mysqli_fetch_assoc($result)){
+					$posts.=post($row['id'],$row['user-id'],$row['image-url'],$row['text-content'],$row['date']);								
+				}
+				return $posts;
 
-			$lastName = $rowInfo["last-name"];
-			$firstName = $rowInfo["first-name"];
-			$imageUrl = $rowInfo["image-url"];
-
-			$query = "SELECT * FROM post WHERE `user-id`= $userId ORDER BY `date` DESC";
-
-			$result = mysqli_query($conn,$query);
-
-			if($result){
-				if(mysqli_num_rows($result) > 0 ){
-
-					while($row = mysqli_fetch_assoc($result)){
-
-						$postImgUrl = $row["image-url"];
-						$postTextContent = $row["text-content"]; 
-
-						echo "<div class='post'>
-							  	<h4>$firstName $lastName</h4>
-							  	<p>$postTextContent</p>
-							  	<img src=$postImgUrl>
-						  	  </div>";
-						echo getNumberOfPostLikes($row['id']);
-						getPostComments($row['id']);
-					}
-
-				} else echo "<h4 class='message'>No posts found</h4>";
-				
-			} else echo mysql_error($conn);
-			 						
+			} else echo "<h4 class='message'>No posts found</h4>";
+			
 		} else echo mysql_error($conn);
+			 						
+		
 
 		mysqli_close($conn);
 	}
